26c4b3b6e2ce93a962fa2ec256f6bc09096478ba
joemersonsouza <joemerson.souza@mercadolivre.com>
2018-12-12 14:31:16 +0000

    Including comments and improvements

--- CryptoShift/app/src/main/java/com/developer/joe/cryptoshift/DBAccess/DbConnection.kt
+++ CryptoShift/app/src/main/java/com/developer/joe/cryptoshift/DBAccess/DbConnection.kt
@@ -4,22 +4,40 @@ import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 
+/**
+* Database connection access
+* 
+* @author Joemerson Souza
+**/
 class DbConnection(
-    context: Context?, name: String?="cryptocoins", factory: SQLiteDatabase.CursorFactory?=null, version: Int=1
+    context: Context?, name: String?="cryptocoins", factory: SQLiteDatabase.CursorFactory?=null, version: Int=0
 ) :  SQLiteOpenHelper(context, name, factory, version) {
 
-    val CREATE_CRYPTO_TABLE = "CREATE TABLE CRYPTOCOINS (SYMBOL VARCHAR(150), PRICE VARCHAR(100));"
-    val DROP_CRYPTO_TABLE = "DROP TABLE CRYPTOCOINS;"
-
+    /**
+    * Method called when database is created
+    *
+    * @param db Instance of database
+    **/
     override fun onCreate(db: SQLiteDatabase?) {
-        db?.execSQL(CREATE_CRYPTO_TABLE)
+        db?.execSQL(DbAction.CREATE_CRYPTO_TABLE)
     }
 
+    /**
+    * Method called when database is updated
+    *
+    * @param db Instance of database
+    * @param fromVersion current version
+    * @param toVersion next version
+    **/
     override fun onUpgrade(db: SQLiteDatabase?, fromVersion: Int, toVersion: Int) {
-        db?.execSQL(DROP_CRYPTO_TABLE)
-        db?.execSQL(CREATE_CRYPTO_TABLE)
+        db?.execSQL(DbAction.DROP_CRYPTO_TABLE)
+        db?.execSQL(DbAction.CREATE_CRYPTO_TABLE)
     }
-
+    
+    /**
+    * Initialize Database Constructor
+    *
+    **/
     fun initializeDB() {
         val db = this.writableDatabase
         onUpgrade(db, 0, 0)
